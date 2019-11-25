9c7fc904f8b53ac719f2a60c13a850dadf76c355
Magnús Sveinsson <magnus10@ru.is>
2019-11-25 23:44:10 +0000

    added test file for TUI

---
+++ Verkefni - Nan Air/Vinnslumappa/Í vinnslu/Forritun/test.py
@@ -0,0 +1,30 @@
+c = '+'
+v = '|'
+h = '-'
+
+filestream = open('Header.txt','r')
+header = []
+for line in filestream:
+    header.append(line.strip('\n'))
+
+print(c + h*78 + c)
+for line in header:
+    print(c + line.center(78,h) + c)
+print(c + h * 78 + c)
+print(v + "Welcome to your NaN Air Manager".center(78) + v)
+print(v+" ".center(78)+v)
+print(v + "To continue, please select an option from the menu.".center(78) + v)
+print( c + h * 78 + c)
+print( v + "1 - Employee management".center(78) + v)
+print( v + "2 - Airplane management".center(78) + v)
+print( v + "3 - Destination management".center(78) + v)
+print( v + "3 - Trip management".center(78) + v)
+for i in range(1, 3):
+    print( v + " " * 78 + v)
+print( c + h * 78 + c)
+
+c = "+"
+h = "-"
+print( c + h*78 + c)
+inputa = input(" "*32 + "Your choice: ")
+print( c + h*78 + c)
