cf60ea7e1194da83ea63fb1df406b19a04b28b6b
Guðmundur Árni <mummimagnusson@gmail.com>
2019-12-05 20:55:56 +0000

    5.12.2019 kl 20:55

---
+++ Verkefni - Nan Air/NaN Air/DataLayer/DL_API.py
@@ -0,0 +1,43 @@
+# Classes and Constants
+from DataLayer import *
+
+
+
+#Classes 
+class DL_API():
+
+    def read_file_TripFilename(self):
+        pass
+
+    def close_file_TripFilename(self):
+        pass
+
+    def createOutput_TripFilename(self):
+        pass
+
+    def read_file_PlaneFilename(self):
+        pass
+
+    def close_file_PlaneFilename(self):
+        pass
+
+    def createOutput_PlaneFilename(self):
+        pass
+
+    def read_file_DestinationFilename(self):
+        pass
+
+    def close_file_DestinationFilename(self):
+        pass
+
+    def createOutput_DestiantionFilename(self):
+        pass
+
+    def read_file_EmployeeFilename(self):
+        pass
+
+    def close_file_EmployeeFilename(self):
+        pass
+
+    def createOutput_EmployeeFilename(self):
+        pass
